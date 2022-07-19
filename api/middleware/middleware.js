e0ee5b582d9374675db749fe776e824853956879
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-07-19 17:48:56 -0600

    logger works

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,28 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timeStamp}] ${method} to ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUserId middleware')
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUser middleware')
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validatePost middleware')
+  next()
 }
-
+module.exports = {logger, validatePost, validateUser, validateUserId}
 // do not forget to expose these functions to other modules
--- api/server.js
+++ api/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const server = express();
-
+const {logger} = require('./middleware/middleware')
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json())
-
 // global middlewares and the user's router need to be connected here
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -14,3 +13,4 @@ server.get('/', (req, res) => {
 module.exports = server;
 
 
+// http :5000
